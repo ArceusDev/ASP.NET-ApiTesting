c7089a17d009412baf2ed97342a9b1ea4ecead0a
Hamza Afzal <ha593164@gmail.com>
2023-10-01 00:38:30 +0500

    added Calculator API

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aspdotnetApiTesting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/<CalculatorController>/5
+        [HttpGet("{firstNumber}, {secondNumber}, {operation}")]
+        public string Get(double firstNumber, double secondNumber, string operation)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "plus":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "minus":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "multiply":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "divide":
+                    result = firstNumber / secondNumber;
+                    break;
+                default:
+                    result = -1;
+                    break;
+            }
+            return result.ToString();
+        }
+    }
+}
